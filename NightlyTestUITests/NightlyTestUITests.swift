ea1591204ce2f7df6f1d1c2cac573c96b554a2ac
David Ametsitsi <david@generalui.com>
2018-12-13 16:01:58 -0800

    add failing test

--- NightlyTestUITests/NightlyTestUITests.swift
+++ NightlyTestUITests/NightlyTestUITests.swift
@@ -27,8 +27,7 @@ class NightlyTestUITests: XCTestCase {
     }
 
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTFail("wow uh oh")
     }
 
 }
